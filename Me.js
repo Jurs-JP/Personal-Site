6dcb7cec534166c4986937288abba9b79d020965
Jurist John Plaza <40057871+Jurs-JP@users.noreply.github.com>
2022-10-26 18:20:42 +0800

    Add files via upload

---
+++ Me.js
@@ -0,0 +1,73 @@
+const buttons = document.querySelectorAll("button");
+const allDivs = document.querySelectorAll("div");
+const allSections = document.querySelectorAll("section");
+const allMain = document.querySelectorAll("main");
+
+
+
+var tagArray = [buttons, allDivs, allSections, allMain];
+
+var borders = () => {
+
+for(var z = 0; z < tagArray.length; z++) {
+
+        for(var i = 0; i <tagArray[z].length; i++) {
+            tagArray[z][i].style.border = "1px solid green";
+            tagArray[z][i].style.boxSizing = "border-box";
+        }
+    }
+    
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+var projectTitles = [];
+var projectTags = [];
+
+var projectObj = {
+    project01: {
+        title: "???",
+        tag: "Esports",
+        addTitle() {
+            projectTitles.push(this.title)
+        }
+    },
+    project02: {
+        title: "???",
+        tag: "Math",
+        addTitle() {
+            projectTitles.push(this.title)
+        }
+    },
+    project03: {
+        title: "3d Dimensions",
+        tag: "Animation",
+        addTitle() {
+            projectTitles.push(this.title)
+        }  
+    }
+}
+
+projectObj.project01.addTitle();
+projectObj.project02.addTitle();
+projectObj.project03.addTitle();
+
+
+
+
+const projBoxes = document.getElementById("projectBox2");
+var projDivs = "";
+
+for(var i =0;i < projectTitles.length; i++) {
+    if(projectTitles.length <= 3) {
+        projDivs += '<div id="projectBoxes"></div>';
+    }
+}
+
+projBoxes.innerHTML = projDivs;
+
+
+
+
+
